2eeb9f1afd7e376966cc8266ff7d2e943d12d614
amrnt <amrnt@users.noreply.github.com>
2022-08-30 17:44:38 +0000

    [auto] generate selling partner api client

--- api/fulfillmentInboundV0/fulfillment_inbound_v0_models/partnered_small_parcel_data_input.go
+++ api/fulfillmentInboundV0/fulfillment_inbound_v0_models/partnered_small_parcel_data_input.go
@@ -18,7 +18,7 @@ import (
 // swagger:model PartneredSmallParcelDataInput
 type PartneredSmallParcelDataInput struct {
 
-	// The Amazon-partnered carrier to use for the inbound shipment.
+	// The Amazon-partnered carrier to use for the inbound shipment. **`CarrierName`** values in France (FR), Italy (IT), Spain (ES), the United Kingdom (UK), and the United States (US): `UNITED_PARCEL_SERVICE_INC`. <br> **`CarrierName`** values in Germany (DE): `DHL_STANDARD`,`UNITED_PARCEL_SERVICE_INC`. <br>Default: `UNITED_PARCEL_SERVICE_INC`.
 	CarrierName string `json:"CarrierName,omitempty"`
 
 	// package list
