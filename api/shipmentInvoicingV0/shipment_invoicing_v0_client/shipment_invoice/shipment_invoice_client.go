a2365a3e1d30c4328e0bcf371d9036b258bd51ff
amrnt <amrnt@users.noreply.github.com>
2023-01-12 05:34:44 +0000

    [auto] generate selling partner api client

--- api/shipmentInvoicingV0/shipment_invoicing_v0_client/shipment_invoice/shipment_invoice_client.go
+++ api/shipmentInvoicingV0/shipment_invoicing_v0_client/shipment_invoice/shipment_invoice_client.go
@@ -42,14 +42,13 @@ type ClientService interface {
 /*
 	GetInvoiceStatus Returns the invoice status for the shipment you specify.
 
-**Usage Plans:**
+**Usage Plan:**
 
-| Plan type | Rate (requests per second) | Burst |
-| ---- | ---- | ---- |
-|Default| 1.133 | 25 |
-|Selling partner specific| Variable | Variable |
+| Rate (requests per second) | Burst |
+| ---- | ---- |
+| 1.133 | 25 |
 
-The x-amzn-RateLimit-Limit response header returns the usage plan rate limits that were applied to the requested operation. Rate limits for some selling partners will vary from the default rate and burst shown in the table above. For more information, see "Usage Plans and Rate Limits" in the Selling Partner API documentation.
+The `x-amzn-RateLimit-Limit` response header returns the usage plan rate limits that were applied to the requested operation, when available. The table above indicates the default rate and burst values for this operation. Selling partners whose business demands require higher throughput may see higher rate and burst values than those shown here. For more information, see [Usage Plans and Rate Limits in the Selling Partner API](doc:usage-plans-and-rate-limits-in-the-sp-api).
 */
 func (a *Client) GetInvoiceStatus(params *GetInvoiceStatusParams, opts ...ClientOption) (*GetInvoiceStatusOK, error) {
 	// TODO: Validate the params before sending
@@ -89,14 +88,13 @@ func (a *Client) GetInvoiceStatus(params *GetInvoiceStatusParams, opts ...Client
 /*
 	GetShipmentDetails Returns the shipment details required to issue an invoice for the specified shipment.
 
-**Usage Plans:**
+**Usage Plan:**
 
-| Plan type | Rate (requests per second) | Burst |
-| ---- | ---- | ---- |
-|Default| 1.133 | 25 |
-|Selling partner specific| Variable | Variable |
+| Rate (requests per second) | Burst |
+| ---- | ---- |
+| 1.133 | 25 |
 
-The x-amzn-RateLimit-Limit response header returns the usage plan rate limits that were applied to the requested operation. Rate limits for some selling partners will vary from the default rate and burst shown in the table above. For more information, see "Usage Plans and Rate Limits" in the Selling Partner API documentation.
+The `x-amzn-RateLimit-Limit` response header returns the usage plan rate limits that were applied to the requested operation, when available. The table above indicates the default rate and burst values for this operation. Selling partners whose business demands require higher throughput may see higher rate and burst values than those shown here. For more information, see [Usage Plans and Rate Limits in the Selling Partner API](doc:usage-plans-and-rate-limits-in-the-sp-api).
 */
 func (a *Client) GetShipmentDetails(params *GetShipmentDetailsParams, opts ...ClientOption) (*GetShipmentDetailsOK, error) {
 	// TODO: Validate the params before sending
@@ -136,14 +134,13 @@ func (a *Client) GetShipmentDetails(params *GetShipmentDetailsParams, opts ...Cl
 /*
 	SubmitInvoice Submits a shipment invoice document for a given shipment.
 
-**Usage Plans:**
+**Usage Plan:**
 
-| Plan type | Rate (requests per second) | Burst |
-| ---- | ---- | ---- |
-|Default| 1.133 | 25 |
-|Selling partner specific| Variable | Variable |
+| Rate (requests per second) | Burst |
+| ---- | ---- |
+| 1.133 | 25 |
 
-The x-amzn-RateLimit-Limit response header returns the usage plan rate limits that were applied to the requested operation. Rate limits for some selling partners will vary from the default rate and burst shown in the table above. For more information, see "Usage Plans and Rate Limits" in the Selling Partner API documentation.
+The `x-amzn-RateLimit-Limit` response header returns the usage plan rate limits that were applied to the requested operation, when available. The table above indicates the default rate and burst values for this operation. Selling partners whose business demands require higher throughput may see higher rate and burst values than those shown here. For more information, see [Usage Plans and Rate Limits in the Selling Partner API](doc:usage-plans-and-rate-limits-in-the-sp-api).
 */
 func (a *Client) SubmitInvoice(params *SubmitInvoiceParams, opts ...ClientOption) (*SubmitInvoiceOK, error) {
 	// TODO: Validate the params before sending
