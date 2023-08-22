d7632ab6e0ef1ad7ca6a98e726249177dcdeeede
amrnt <amrnt@users.noreply.github.com>
2023-08-22 05:34:41 +0000

    [auto] generate selling partner api client

--- api/productPricingV0/product_pricing_v0_client/product_pricing/product_pricing_client.go
+++ api/productPricingV0/product_pricing_v0_client/product_pricing/product_pricing_client.go
@@ -146,9 +146,9 @@ func (a *Client) GetItemOffers(params *GetItemOffersParams, opts ...ClientOption
 
 | Rate (requests per second) | Burst |
 | ---- | ---- |
-| 0.5 | 1 |
+| 0.1 | 1 |
 
-The `x-amzn-RateLimit-Limit` response header returns the usage plan rate limits that were applied to the requested operation, when available. The table above indicates the default rate and burst values for this operation. Selling partners whose business demands require higher throughput may see higher rate and burst values than those shown here. For more information, see [Usage Plans and Rate Limits in the Selling Partner API](doc:usage-plans-and-rate-limits-in-the-sp-api).
+The `x-amzn-RateLimit-Limit` response header returns the usage plan rate limits that were applied to the requested operation, when available. The table above indicates the default rate and burst values for this operation. Selling partners whose business demands require higher throughput may see higher rate and burst values than those shown here. For more information, see [Usage Plans and Rate Limits in the Selling Partner API](https://developer-docs.amazon.com/sp-api/docs/usage-plans-and-rate-limits-in-the-sp-api).
 */
 func (a *Client) GetItemOffersBatch(params *GetItemOffersBatchParams, opts ...ClientOption) (*GetItemOffersBatchOK, error) {
 	// TODO: Validate the params before sending
