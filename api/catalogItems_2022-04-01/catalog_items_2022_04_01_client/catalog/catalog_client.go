8a74d887acd6ad6abd27cfa53eea020b2ec83f00
amrnt <amrnt@users.noreply.github.com>
2024-08-26 17:35:35 +0000

    [auto] generate selling partner api client

--- api/catalogItems_2022-04-01/catalog_items_2022_04_01_client/catalog/catalog_client.go
+++ api/catalogItems_2022-04-01/catalog_items_2022_04_01_client/catalog/catalog_client.go
@@ -44,7 +44,7 @@ type ClientService interface {
 
 | Rate (requests per second) | Burst |
 | ---- | ---- |
-| 5 | 5 |
+| 2 | 2 |
 
 The `x-amzn-RateLimit-Limit` response header returns the usage plan rate limits that were applied to the requested operation, when available. The table above indicates the default rate and burst values for this operation. Selling partners whose business demands require higher throughput may observe higher rate and burst values than those shown here. For more information, refer to the [Usage Plans and Rate Limits in the Selling Partner API](doc:usage-plans-and-rate-limits-in-the-sp-api).
 */
@@ -90,7 +90,7 @@ func (a *Client) GetCatalogItem(params *GetCatalogItemParams, opts ...ClientOpti
 
 | Rate (requests per second) | Burst |
 | ---- | ---- |
-| 5 | 5 |
+| 2 | 2 |
 
 The `x-amzn-RateLimit-Limit` response header returns the usage plan rate limits that were applied to the requested operation, when available. The table above indicates the default rate and burst values for this operation. Selling partners whose business demands require higher throughput may observe higher rate and burst values than those shown here. For more information, refer to the [Usage Plans and Rate Limits in the Selling Partner API](doc:usage-plans-and-rate-limits-in-the-sp-api).
 */
