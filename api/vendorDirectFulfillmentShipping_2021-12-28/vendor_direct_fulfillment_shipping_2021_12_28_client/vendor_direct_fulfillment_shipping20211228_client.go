98541bc93a4935dc1c7716ab131d60d236f8425f
amrnt <amrnt@users.noreply.github.com>
2024-11-27 05:41:08 +0000

    [auto] generate selling partner api client

--- api/vendorDirectFulfillmentShipping_2021-12-28/vendor_direct_fulfillment_shipping_2021_12_28_client/vendor_direct_fulfillment_shipping20211228_client.go
+++ api/vendorDirectFulfillmentShipping_2021-12-28/vendor_direct_fulfillment_shipping_2021_12_28_client/vendor_direct_fulfillment_shipping20211228_client.go
@@ -10,7 +10,6 @@ import (
 	httptransport "github.com/go-openapi/runtime/client"
 	"github.com/go-openapi/strfmt"
 
-	"github.com/renabled/amzn-sp-api-go/api/vendorDirectFulfillmentShipping_2021-12-28/vendor_direct_fulfillment_shipping_2021_12_28_client/create_container_label"
 	"github.com/renabled/amzn-sp-api-go/api/vendorDirectFulfillmentShipping_2021-12-28/vendor_direct_fulfillment_shipping_2021_12_28_client/customer_invoices"
 	"github.com/renabled/amzn-sp-api-go/api/vendorDirectFulfillmentShipping_2021-12-28/vendor_direct_fulfillment_shipping_2021_12_28_client/vendor_shipping"
 	"github.com/renabled/amzn-sp-api-go/api/vendorDirectFulfillmentShipping_2021-12-28/vendor_direct_fulfillment_shipping_2021_12_28_client/vendor_shipping_labels"
@@ -58,7 +57,6 @@ func New(transport runtime.ClientTransport, formats strfmt.Registry) *VendorDire
 
 	cli := new(VendorDirectFulfillmentShipping20211228)
 	cli.Transport = transport
-	cli.CreateContainerLabel = create_container_label.New(transport, formats)
 	cli.CustomerInvoices = customer_invoices.New(transport, formats)
 	cli.VendorShipping = vendor_shipping.New(transport, formats)
 	cli.VendorShippingLabels = vendor_shipping_labels.New(transport, formats)
@@ -106,8 +104,6 @@ func (cfg *TransportConfig) WithSchemes(schemes []string) *TransportConfig {
 
 // VendorDirectFulfillmentShipping20211228 is a client for vendor direct fulfillment shipping20211228
 type VendorDirectFulfillmentShipping20211228 struct {
-	CreateContainerLabel create_container_label.ClientService
-
 	CustomerInvoices customer_invoices.ClientService
 
 	VendorShipping vendor_shipping.ClientService
@@ -120,7 +116,6 @@ type VendorDirectFulfillmentShipping20211228 struct {
 // SetTransport changes the transport on the client and all its subresources
 func (c *VendorDirectFulfillmentShipping20211228) SetTransport(transport runtime.ClientTransport) {
 	c.Transport = transport
-	c.CreateContainerLabel.SetTransport(transport)
 	c.CustomerInvoices.SetTransport(transport)
 	c.VendorShipping.SetTransport(transport)
 	c.VendorShippingLabels.SetTransport(transport)
